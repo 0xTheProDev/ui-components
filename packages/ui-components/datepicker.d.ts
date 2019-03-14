762e6f8928368b50abc73e414d2e6e0ec365b9e2
sendgrid-ci <jenkins-ci@sengrid.com>
2019-03-14 21:13:02 +0000

    [Prepublish Built]

---
+++ packages/ui-components/datepicker.d.ts
@@ -0,0 +1,3 @@
+import 'react-dates/initialize';
+export { DateRangePicker, SingleDatePicker } from 'react-dates';
+import './styles/datepicker.css';
---
+++ packages/ui-components/datepicker.js
@@ -0,0 +1,4 @@
+// need to import initialize for the datepicker to work
+import 'react-dates/initialize';
+export { DateRangePicker, SingleDatePicker } from 'react-dates';
+import './styles/datepicker.css';
