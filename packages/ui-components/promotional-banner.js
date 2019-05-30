1b841ee71fdb89b3bbc4b60c54e52d3680f70c75
sendgrid-ci <jenkins-ci@sengrid.com>
2019-05-30 17:41:14 +0000

    [Prepublish Built]

--- packages/ui-components/promotional-banner.js
+++ packages/ui-components/promotional-banner.js
@@ -4,13 +4,14 @@ import Icon from './icon';
 import Styles from './styles/promotional-banner.module.scss';
 export const PromotionalBanner = ({ actions, className, learnMore, title, description, onClickX, image, }) => {
     return (React.createElement("div", { className: cn({ [className]: !!className }, Styles.banner, 'promotional-banner-wrap') },
-        React.createElement("div", { className: cn(Styles['image-wrap'], 'image-wrap') }, image),
-        React.createElement("div", { className: Styles['main-content'] },
-            React.createElement("div", null,
-                React.createElement("h1", null, title),
-                React.createElement("p", null, description)),
-            React.createElement("div", { className: cn(Styles.actions, 'promotional-banner-actions') },
-                learnMore && (React.createElement("span", { className: Styles['learn-more'] }, learnMore)),
-                actions)),
+        React.createElement("div", { className: Styles['centered-content'] },
+            React.createElement("div", { className: cn(Styles['image-wrap'], 'image-wrap') }, image),
+            React.createElement("div", { className: Styles['main-content'] },
+                React.createElement("div", null,
+                    React.createElement("h1", null, title),
+                    React.createElement("p", null, description)),
+                React.createElement("div", { className: cn(Styles.actions, 'promotional-banner-actions') },
+                    learnMore && (React.createElement("span", { className: Styles['learn-more'] }, learnMore)),
+                    actions))),
         onClickX && React.createElement(Icon, { type: "x", onClick: onClickX })));
 };
