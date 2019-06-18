f6ad8f59a4f25d2b68b2d06347a1c528ef0853ed
sendgrid-ci <jenkins-ci@sengrid.com>
2019-06-18 18:56:52 +0000

    [Prepublish Built]

--- packages/ui-components/datepicker.d.ts
+++ packages/ui-components/datepicker.d.ts
@@ -1,3 +1,19 @@
+import moment from 'moment';
+import { Component } from 'react';
+import { SingleDatePickerShape } from 'react-dates';
 import 'react-dates/initialize';
-export { DateRangePicker, SingleDatePicker, DayPickerRangeController, } from 'react-dates';
 import './styles/datepicker.css';
+export { DateRangePicker, SingleDatePicker, DayPickerRangeController, } from 'react-dates';
+interface SingleDatePickerWithRangeProps {
+    startYear?: number;
+    endYear?: number;
+}
+export declare class SingleDatePickerWithRange extends Component<SingleDatePickerWithRangeProps & SingleDatePickerShape> {
+    render(): JSX.Element;
+    renderMonthElement({ month, onMonthSelect, onYearSelect, }: {
+        month: moment.Moment;
+        onMonthSelect: (currentMonth: moment.Moment, newMonthVal: string) => void;
+        onYearSelect: (currentYear: moment.Moment, newYearVal: string) => void;
+    }): JSX.Element;
+}
+export default SingleDatePickerWithRange;
--- packages/ui-components/datepicker.js
+++ packages/ui-components/datepicker.js
@@ -1,4 +1,33 @@
+import { omit, range } from 'lodash';
+import moment from 'moment';
+import React, { Component } from 'react';
+import { SingleDatePicker } from 'react-dates';
 // need to import initialize for the datepicker to work
 import 'react-dates/initialize';
-export { DateRangePicker, SingleDatePicker, DayPickerRangeController, } from 'react-dates';
+import Icon from './icon';
+import Select from './select';
+import Styles from './styles/datepicker-months.scss';
 import './styles/datepicker.css';
+export { DateRangePicker, SingleDatePicker, DayPickerRangeController, } from 'react-dates';
+export class SingleDatePickerWithRange extends Component {
+    render() {
+        const { id, focused, onFocusChange, date, onDateChange } = this.props;
+        return (React.createElement("div", { className: Styles['datepicker-container'] },
+            React.createElement(SingleDatePicker, Object.assign({}, omit(this.props, 'startYear', 'endYear'), { id: id, date: date, onDateChange: onDateChange, focused: focused, onFocusChange: onFocusChange, renderMonthElement: monthProps => this.renderMonthElement(monthProps), daySize: 50, showClearDate: true, customCloseIcon: React.createElement(Icon, { type: "x", className: Styles['datepicker-clear-icon'] }) }))));
+    }
+    renderMonthElement({ month, onMonthSelect, onYearSelect, }) {
+        const currentYear = moment().year();
+        const { startYear = 1900, endYear = currentYear + 50 } = this.props;
+        const months = moment
+            .months()
+            .map((label, value) => ({ label, value: value.toString() }));
+        const years = range(startYear, endYear).map(label => ({
+            label: label.toString(),
+            value: label.toString(),
+        }));
+        return (React.createElement("div", { className: Styles['datepicker-month-container'] },
+            React.createElement(Select, { value: months.find(m => m.value === month.month().toString()), onChange: ({ value }) => onMonthSelect(month, value), options: months, className: "input-select-wrap" }),
+            React.createElement(Select, { value: years.find(y => y.value === month.year().toString()), onChange: ({ value }) => onYearSelect(month, value), options: years, className: "input-select-wrap" })));
+    }
+}
+export default SingleDatePickerWithRange;
